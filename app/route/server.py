cd7699160f09f2dabaabb4a931f416decbcdb870
ishaikhoon <ishaikhoon@gmail.com>
2022-12-15 10:06:40 +0300

    sign in API

--- app/route/server.py
+++ app/route/server.py
@@ -1,5 +1,8 @@
 # server.py
 from fastapi import FastAPI
+from app.database import addUser
+
+from app.route.models import User
 
 app = FastAPI()
 
@@ -7,5 +10,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello WocxsaxZrld"}
+
+@app.post("/signup")
+async def signup(Fname:str , Lname:str , phone:str):
+    await addUser(Fname,Lname,phone)
 
     
